e293ef607dd4b9d5ac506289d11f7b287249ee31
Sherry <guoyunfei03@163.com>
2017-09-16 20:19:20 +0800

    First

---
+++ getArrayMax.java
@@ -0,0 +1,18 @@
+package com.Sherry;
+
+/**
+ * Created by lenovo on 2017-01-23.
+ * 取数组最大值
+ * 遍历数组
+ */
+public class getArrayMax {
+    public int getMax(int[] input){
+        int last = input[0];
+        for (int i:input){
+            if(i > last){
+                last = i;
+            }
+        }
+        return last;
+    }
+}
